81900effd11be0232dc1e9b0714b93013757655a
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-07-24 21:05:24 +0300

    Update info.java

--- ObjectRecognitionModel/app/src/main/java/com/example/bs/objectrecognitionmodel/info.java
+++ ObjectRecognitionModel/app/src/main/java/com/example/bs/objectrecognitionmodel/info.java
@@ -1,15 +1,36 @@
 package com.example.bs.objectrecognitionmodel;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class info extends Activity {
+    private TextToSpeech TTS;
+
+    @Override
+    public void onConfigurationChanged(Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+        setContentView(R.layout.activity_info);
+        change();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        change();
+    }
+
+    private void change(){
 
         //get the screen size to set the width & height of pop-up
         DisplayMetrics dm = new DisplayMetrics();
